1c31c2192e2c91b1175f4337ca511b81fa563021
Jeff Tucker <trydionel@gmail.com>
2009-12-23 20:17:36 -0500

    Blinky cursor thing\!

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,21 @@
+var blink = function(element, speed) {
+  if (speed === undefined) {
+    speed = 50;
+  }
+  
+  var antispeed = 1000 - speed;
+  element.fadeOut(speed, function() {
+    setTimeout(function() { element.fadeIn(speed, function() {
+      setTimeout(function() { blink(element) }, antispeed)
+    }) }, antispeed);
+  });
+};
+
 $(document).ready(function() {
   $(".toggleable").not(".open").hide();
+  
+  // Obnoxious blinky cursor!
+  blink($(".cursor"));
   
   $(".toggle-handler").live("click", function() {
     $(this).children(".toggleable").toggle("blind");
@@ -7,6 +23,6 @@ $(document).ready(function() {
   
   $(".activate-keywords").bind("click", function(e) {
     e.preventDefault();
-    $("body").toggleClass("highlight-keywords", 1000);
+    $("#content").toggleClass("highlight-keywords", 1000);
   });
 });
