63873eae5c4a24d476562803573d10d6ad84159c
Jeff Tucker <trydionel@gmail.com>
2009-12-23 20:16:51 -0500

    Adding a few view helpers, improving compass config

--- resume.rb
+++ resume.rb
@@ -1,17 +1,31 @@
 require 'rubygems'
 require 'compass'
+require 'compass-colors'
+require 'fancy-buttons'
 require 'sinatra'
 require 'haml'
 require 'sass'
+require 'md5'
 
 configure do
   Compass.configuration do |config|
     config.project_path = File.dirname(__FILE__)
     config.sass_dir = 'views'
+    config.images_dir = '/'
   end
   
   set :haml, { :format => :html5, :layout => :'layouts/application' }
   set :sass, Compass.sass_engine_options
+end
+
+helpers do
+  def gravatar_link(email, size=80)
+    "http://www.gravatar.com/avatar/#{MD5::md5(email.downcase)}?s=#{size}"
+  end
+end
+
+get '/gravatar' do
+  gravatar_link("trydionel@gmail.com")
 end
 
 get '/stylesheets/:name.css' do
